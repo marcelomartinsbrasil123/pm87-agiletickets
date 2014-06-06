c55a67370ed4480205399ac59343792c918119f7
MarceloMartins <marcelo.martins@tjdft.jus.br>
2014-06-06 10:48:22 -0300

    testando o jenkins 2

--- src/test/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecosTest.java
+++ src/test/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecosTest.java
@@ -21,7 +21,7 @@ public class CalculadoraDePrecosTest {
 		
 		BigDecimal precoTotal = CalculadoraDePrecos.calcula(sessao, 9);
 		
-		assertEquals(2, BigDecimal.valueOf(90.0).compareTo(precoTotal));
+		assertEquals(0, BigDecimal.valueOf(90.0).compareTo(precoTotal));
 	}
 	
 	@Test
